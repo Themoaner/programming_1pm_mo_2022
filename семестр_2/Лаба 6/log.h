efb3a82c6d42aae4688748e8ac4cef818f90db7e
themoaner <72627928+Themoaner@users.noreply.github.com>
2023-05-27 00:11:30 +0200

    Create log.h

---
+++ семестр_2/Лаба 6/log.h
@@ -0,0 +1,74 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <chrono>
+
+enum class LogLevel
+{
+	DEBUG,
+	RELEASE
+};
+
+class Log
+{
+private:
+	static std::ofstream m_out;
+	static LogLevel m_logLevel;
+
+public:
+	Log()
+	{
+		std::cout << "Constructor" << std::endl;
+	}
+
+	static void SetLogLevel(LogLevel logLevel)
+	{
+		m_logLevel = logLevel;
+	}
+
+	static void SetLogPath(const std::string& path)
+	{
+		m_out.open(path);
+	}
+
+	static void Write(const std::string& msg)
+	{
+		//time_t now = time(0);
+		//char* dt = ctime(&now);
+		if (m_logLevel == LogLevel::DEBUG)
+		{
+			std::cerr << /*dt <<*/ msg << std::endl;
+		}
+		m_out << /*dt <<*/ msg << std::endl;
+		m_out.flush();
+	}
+
+	static void INFO(const std::string& msg)
+	{
+		//time_t now = time(0);
+		//char* dt = ctime(&now);
+		if (m_logLevel == LogLevel::DEBUG)
+		{
+			std::cerr << msg << std::endl;
+		}
+		m_out << /*dt <<*/ msg << std::endl;
+		m_out.flush();
+	}
+
+	static void ERROR(const std::string& msg)
+	{
+		//time_t now = time(0);
+		//char* dt = ctime(&now);
+		if (m_logLevel == LogLevel::DEBUG)
+		{
+			std::cerr << msg << std::endl;
+		}
+		m_out << /*dt <<*/ msg << std::endl;
+		m_out.flush();
+	}
+
+	~Log()
+	{
+		std::cout << "Destructor" << std::endl;
+	}
+};
