f10698e4389dc97d118ed91442f68bc406a91d28
themoaner <72627928+Themoaner@users.noreply.github.com>
2023-06-30 16:42:36 +0200

    Create Game.h

---
+++ project2/final_susa/Game.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include "Map.h"
+#include "Entity.h"
+
+namespace mt {
+    class Game {
+    private:
+        int m_width = 0;
+        int m_height = 0;
+        std::string m_caption;
+        sf::RenderWindow* m_window = nullptr;
+
+    public:
+        Game() = default;
+        ~Game();
+
+        void SetCaption(const std::string& caption);
+
+        void SetResolution(int width, int height);
+
+        void Setup();
+
+        void Run();
+    };
+}
