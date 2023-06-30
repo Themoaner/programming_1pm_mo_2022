57fa0aebfc9e8ddb04a5249961e3f06b2648c26c
themoaner <72627928+Themoaner@users.noreply.github.com>
2023-06-30 16:44:09 +0200

    Create Map.h

---
+++ project2/final_susa/Map.h
@@ -0,0 +1,38 @@
+#include <SFML/Graphics.hpp>
+#pragma once
+#include <fstream>
+#include <iostream>
+
+
+namespace m {
+    class Map {
+    private:
+        void SetSize();
+    public:
+        Map(std::string path);
+
+        ~Map();
+
+        void SetMassive();
+
+        void SetImage();
+
+        ////////ЗАДАНИЕ ТЕКСТУРЫ КАРТЫ////////
+        sf::Sprite* SetTexture(int a, int b);
+
+        //Геттеры
+        char** GetMassive();
+        int GetHEIGHT();
+        int GetWIDTH();
+        sf::Sprite* GetSprite();
+
+    private:
+        std::ifstream map_file;
+        std::ofstream out;
+        int map_HEIGHT;
+        int map_WIDTH;
+        char** map_mas;
+        sf::Texture map_texture;
+        sf::Sprite map_sprite;//создаём спрайт для карты
+    };
+}
